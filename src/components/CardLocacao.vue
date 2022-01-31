a306aaaa1d0cd8bea9f708fcc2782e76858d09bf
marcos-bah <marcos.barbosa@unfei.edu.br>
2022-01-31 20:21:34 -0300

    Fix descricao

--- src/components/CardLocacao.vue
+++ src/components/CardLocacao.vue
@@ -50,7 +50,7 @@
             <!-- limitar 20 caracteres -->
 
             {{
-              (locacao.endereco || "Não informado").substring(0, 40) + "..." ||
+              (locacao.descricao || "Não informado").substring(0, 40) + "..." ||
               "Não informado"
             }}
 
